611262b7a6ac269f8f3958c61cad34beb886f706
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2026-05-16 08:42:39 +0530

    Create Question4.sql

---
+++ Week2/Assignment_StrDataNum/Question4.sql
@@ -0,0 +1,28 @@
+
+SELECT
+
+    user_id,
+
+    user_email,
+
+    SUBSTRING_INDEX(user_email,'@',-1) AS email_domain,
+
+    TIMESTAMPDIFF(MONTH,start_date,end_date) AS duration_months,
+
+    FORMAT(subscription_fee,2) AS formatted_fee,
+
+    DATEDIFF(end_date,CURDATE()) AS remaining_days,
+
+    CASE
+
+        WHEN end_date < CURDATE()
+            THEN 'Expired'
+
+        WHEN DATEDIFF(end_date,CURDATE()) <= 30
+            THEN 'Expiring Soon'
+
+        ELSE 'Active'
+
+    END AS subscription_status
+
+FROM subscriptions;
