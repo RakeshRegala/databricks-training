4bd77e23d7c69d3c9a124964d10ae2cc5b324ca1
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2026-05-16 07:48:15 +0530

    Update Assignment_StrDataNum.sql

--- Week2/Assignment_StrDataNum/Assignment_StrDataNum.sql
+++ Week2/Assignment_StrDataNum/Assignment_StrDataNum.sql
@@ -1,4 +1,35 @@
 --· Convert emp_name to proper case ---upper /lower ---Initcap (CamelCase)
 
-select UPPER(emp_name) as UPPERCASE
-from employee_payments
+--select UPPER(emp_name) as UPPERCASE
+--from employee_payments
+
+--select LOWER(emp_name) as LOWERCASE
+--from employee_payments--
+
+
+SELECT 
+    emp_id,
+
+    emp_name,
+
+    UPPER(emp_name) AS upper_name,
+
+    LOWER(emp_name) AS lower_name,
+
+    base_salary + IFNULL(bonus,0) AS total_income,
+
+    ROUND(base_salary + IFNULL(bonus,0)) AS rounded_income,
+
+    YEAR(joining_date) AS joining_year,
+
+    CASE
+        WHEN TIMESTAMPDIFF(YEAR, joining_date, CURDATE()) > 7
+            THEN 'Senior'
+
+        WHEN TIMESTAMPDIFF(YEAR, joining_date, CURDATE()) BETWEEN 4 AND 7
+            THEN 'Mid'
+
+        ELSE 'Junior'
+    END AS employee_level
+
+FROM employee_payments;
